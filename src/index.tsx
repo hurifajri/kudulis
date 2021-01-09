c05fa75388f3873f16cc0a298c95d9af8732f133
Muhamad Fajri <hurifajri@gmail.com>
2021-01-09 22:57:23 +0700

    Remove unused files

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.querySelector('#root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
